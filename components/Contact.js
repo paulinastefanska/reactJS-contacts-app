875e1e56d44780de9e59128da364fb2475761c43
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-07-16 11:17:27 +0200

    Add styles

--- components/Contact.js
+++ components/Contact.js
@@ -10,8 +10,8 @@ var Contact = React.createClass({
           className: 'contactImage',
           src: 'http://icons.veryicon.com/png/System/Qetto%202/contacts.png'
         }),
-        React.createElement('p', {className: 'contactLabel'}, 'Imię: ' + this.props.item.firstName),
-        React.createElement('p', {className: 'contactLabel'}, 'Nazwisko: ' + this.props.item.lastName),
+        React.createElement('p', {className: 'contactLabel'}, 'First name: ' + this.props.item.firstName),
+        React.createElement('p', {className: 'contactLabel'}, 'Last name: ' + this.props.item.lastName),
         React.createElement('a', {className: 'contactEmail', href: 'mailto:' + this.props.item.email},
           this.props.item.email
         )
